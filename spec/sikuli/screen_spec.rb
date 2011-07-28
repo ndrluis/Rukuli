b3de0680862af5402b2614a25c57f355ce3443b6
Konstantin Shabanov <etehtsea@gmail.com>
2011-07-28 16:57:11 +0800

    screen size is dynamic

--- spec/sikuli/screen_spec.rb
+++ spec/sikuli/screen_spec.rb
@@ -5,6 +5,8 @@ describe Sikuli::Screen, 'when initialized' do
   
   its(:x) { should == 0 }
   its(:y) { should == 0 }
-  its(:width) { should == 1680 } # screen's resolution
-  its(:height) { should == 1050 } # screen's resolution
+  its(:width) { should > 0 } # screen's resolution
+  its(:width) { should be_instance_of Fixnum }
+  its(:height) { should > 0 } # screen's resolution
+  its(:height) { should be_instance_of Fixnum }
 end
