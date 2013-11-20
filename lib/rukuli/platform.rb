be5a647298103dfe68505a8a0076033a0408017d
Andre Anastacio <andreluisanastacio@gmail.com>
2013-11-19 22:38:34 -0200

    fix error message

--- lib/rukuli/platform.rb
+++ lib/rukuli/platform.rb
@@ -4,7 +4,7 @@ module Rukuli
     def self.sikuli_script_path
       path = "#{ENV['SIKULI_HOME']}/sikuli-script.jar"
       if ENV['SIKULI_HOME'].nil?
-        raise LoadError, "Failed to load '#{path}'\nIs Sikuli installed?"
+        raise LoadError, "Failed to load 'sikuli-script.jar'\nMake sure SIKULI_HOME is set!"
       end
       path
     end
